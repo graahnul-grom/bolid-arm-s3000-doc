b4559293b9d13d9d3bd5808fa3e39287b4423627
dmn <dmn@vbdeb-b>
2023-10-13 09:31:55 +0300

    bin: continue publishing script

--- bin/deb_soft_inst.sh
+++ bin/deb_soft_inst.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-sudo apt install -V
+sudo apt install -V \
     lmodern \
     latex-fonts-arundina \
     fonts-mathjax fonts-mathjax-extras \
     wkhtmltopdf \
     latexmk \
-    tectonic \
     groff \
     texlive-xetex \
     texlive-luatex \
     context
+
+    # tectonic # not found
 
--- bin/publish.sh
+++ bin/publish.sh
@@ -10,7 +10,22 @@ mkdir -p $DEST || { echo ".. !mkdir DEST"; exit 1; }
 [ -r $SRC ] || { echo ".. !SRC"; exit 1; }
 
 # FAIL: sed -E -e 's,<!--.*?-->,,g' $SRC | less
-
 # https://stackoverflow.com/questions/4055837/delete-html-comment-tags-using-regexp
-sed -e :a -re 's/<!--.*?-->//g;/<!--/N;//ba' $SRC > $DEST/0-$NAME.md
+sed -e :a -re 's/<!--.*?-->//g;/<!--/N;//ba' \
+    $SRC > \
+    $DEST/0-$NAME.md
+
+# TODO: don't replace '-' in lists
+#
+sed -E \
+    -e 's,\*\*iso\*\*,**ИСО Орион**,g' \
+    -e 's,\*\*pro\*\*,**АРМ Орион Про**,g' \
+    -e 's,\*\*s3k\*\*,**АРМ С3000**,g' \
+    -e 's,\*\*s2k-eth\*\*,**С2000-Ethernet**,g' \
+    -e 's,\*\*s2km\*\*,**С2000М**,g' \
+    -e 's,\*\*s2km2\*\*,**С2000М исп. 02**,g' \
+    -e 's/-/—/g' \
+    -e 's,(^| )",\1«,g' -e 's,"( |$),»\1,g' \
+    $DEST/0-$NAME.md > \
+    $DEST/1-$NAME.md
 
