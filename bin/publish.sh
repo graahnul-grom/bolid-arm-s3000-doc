f74da8e5e425e15db80707d65b9cabc76eb74274
dmn <dmn@deb-d>
2023-10-13 08:16:59 +0300

    bin: remove html comments

--- bin/publish.sh
+++ bin/publish.sh
@@ -9,3 +9,8 @@ DEST=$REPO/pub
 mkdir -p $DEST || { echo ".. !mkdir DEST"; exit 1; }
 [ -r $SRC ] || { echo ".. !SRC"; exit 1; }
 
+# FAIL: sed -E -e 's,<!--.*?-->,,g' $SRC | less
+
+# https://stackoverflow.com/questions/4055837/delete-html-comment-tags-using-regexp
+sed -e :a -re 's/<!--.*?-->//g;/<!--/N;//ba' $SRC > $DEST/0-$NAME.md
+
